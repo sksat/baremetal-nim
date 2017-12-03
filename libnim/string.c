beb172a8d652f77d22fae20129241ab0d4d0f4fb
sksat <sksat.tec@gmail.com>
2017-12-03 18:10:10 +0900

    impl memcpy,memset

--- libnim/string.c
+++ libnim/string.c
@@ -4,3 +4,25 @@ size_t strlen(const char *str){
 	for(n=0; *str!='\0'; str++, n++);
 	return n;
 }
+
+void *memcpy(void *s1, const void *s2, size_t n){
+    char        *p1 = (char *)s1;
+    const char  *p2 = (const char *)s2;
+
+    while (n-- > 0) {
+        *p1 = *p2;
+        p1++;
+        p2++;
+    }
+    return s1;
+}
+
+void *memset(void *s, int c, size_t n){
+    const unsigned char uc = c;
+    unsigned char       *p = (unsigned char *)s;
+
+    while (n-- > 0)
+        *p++ = uc;
+
+    return s;
+}
--- libnim/string.h
+++ libnim/string.h
@@ -1,3 +1,5 @@
 #include "stddef.h"
 typedef unsigned short wchar_t;
 size_t strlen(const char *str);
+void *memcpy(void *s1, const void *s2, size_t n);
+void *memset(void *s, int c, size_t n);
